bd4bff3d87e3e9353e2c8ebd5ed0fc165b039c5f
ansmor <ansmor21@bergen.org>
2023-03-05 20:53:50 -0600

    less padding in header on smaller screens

--- front-end/src/Header.js
+++ front-end/src/Header.js
@@ -6,7 +6,7 @@ import './App.css';
 
 function Header() {
     return (
-        <Card className="header p-2 header-card">
+        <Card className="header header-card">
             <Card.Title  className="text-center title">CourseConnects</Card.Title>
             <Card.Text className="text-center mb-1 subtitle">
             Find the GroupMe for each of your UT courses!
